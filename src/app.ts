d28cf0475c720036961df7135fc3e8d0fc2b53ae
Rashid Narikkodan <rashidnarikkodan20@gmail.com>
2025-12-18 13:23:24 +0530

    feat - page not found error middlwar addedd

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express,{Response,Request} from "express";
 import path from "path";
 import productRoutes from './routes/inventory.routes'
 
@@ -13,5 +13,6 @@ app.set('views', path.join(__dirname, '../views'));
 
 /* Temporary test route */
 app.use('/', productRoutes)
-
-
+app.use((req:Request,res:Response):void=>{
+    res.status(404).json({success:true,message:'Page Not Found'})
+})
